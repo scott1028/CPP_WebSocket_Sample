2f885ac710a75ce1c184a73d1a53d38173c06238
scott1028 <mic1028002@gmail.com>
2013-11-06 11:55:34 +0800

    modified:   cpp_sha1_base64.cpp 	modified:   python_base64_sha1.py

--- cpp_sha1_base64.cpp
+++ cpp_sha1_base64.cpp
@@ -1,54 +1,54 @@
 #include "base64/base64.h"
 #include <iostream>
 #include "TinySHA1/TinySHA1.hpp"
-#include "openssl/sha.h"
 #include <string>
+#include <vector>
+#include <iomanip>
 
-using namespace std;
+using namespace std; 
+
 // gcc-win.exe cpp_base64_sha1.cpp base64/base64.cpp -lstdc++ && ./a.exe
 
 int main(int argc, char const *argv[])
 {
 	// sha1
-	const std::string val = "scott";
- 	sha1::SHA1 s;
+	const string val = "scott";
+	sha1::SHA1 s;
 	s.processBytes(val.c_str(), val.size());
 	uint32_t digest[5];
 	s.getDigest(digest);
 	char buffer [48];
 	sprintf(buffer,"%08x%08x%08x%08x%08x", digest[0], digest[1], digest[2], digest[3], digest[4]);
 	string sha1_result(buffer);
-	cout << sha1_result << endl;
 
-	int len=21;
+	int len=20;
 	int idx=0;
 	unsigned char sha1[len];
-	sha1[20]='\0';
 
 	// 將各單位 4 Bytes 的陣列轉換為各單位 1 Bytes的陣列
+	// 4278190080 數字很大所以要加 UL, 不然會有 warning
 	cout << endl;
 	for(int i=0;i<5;i++){
-		// cout << hex << digest[i] << endl;
 		// 用 & 跟 >> 計算
-		cout << hex << (unsigned)( ( digest[i] & (unsigned)4278190080 )>>24 ) << " ";
-		cout << hex << (unsigned)( ( digest[i] & 16711680 )>>16 ) << " ";
-		cout << hex << (unsigned)( ( digest[i] & 65280 )>>8 ) << " ";
-		cout << hex << (unsigned)( ( digest[i] & 255 )>>0 ) << " ";
-		sha1[idx]=(unsigned)( ( digest[i] & 4278190080 )>>24 );
-		sha1[++idx]=(unsigned)( ( digest[i] & 16711680 )>>16 );
-		sha1[++idx]=(unsigned)( ( digest[i] & 65280 )>>8 );
-		sha1[++idx]=(unsigned)( ( digest[i] & 255 )>>0 );
+		cout << setfill('0') << setw(2) << hex << (unsigned)( ( digest[i] & 4278190080UL )>>24 ) << " ";
+		cout << setfill('0') << setw(2) << hex << (unsigned)( ( digest[i] & 16711680 )>>16 ) << " ";
+		cout << setfill('0') << setw(2) << hex << (unsigned)( ( digest[i] & 65280 )>>8 ) << " ";
+		cout << setfill('0') << setw(2) << hex << (unsigned)( ( digest[i] & 255 )>>0 ) << " ";
+		sha1[idx]=(unsigned)( ( digest[i] & 4278190080UL )>>24 );//cout << idx;
+		sha1[++idx]=(unsigned)( ( digest[i] & 16711680 )>>16 );//cout << idx;
+		sha1[++idx]=(unsigned)( ( digest[i] & 65280 )>>8 );//cout << idx;
+		sha1[++idx]=(unsigned)( ( digest[i] & 255 )>>0 );//cout << idx;
+		idx++;
 	}
 	cout << endl;
+	cout << endl << "以下為 C++: " << endl;	
 
 	// base64
-	// string s2(sha1);
-	string encoded = base64_encode(reinterpret_cast<const unsigned char*>( sha1 ), len-1 );
-	// // string encoded = base64_encode(reinterpret_cast<const unsigned char*>( sha1 ), 20 );
-	cout << "encoded: " << encoded << endl;
+	string encoded = base64_encode(reinterpret_cast<const unsigned char*>( sha1 ), len );
+	cout << "原先('scott' sha1 then base64): " << encoded << endl;
 
-	// const string tt(sha1);
-	// cout << base64_encode(reinterpret_cast<const unsigned char*>(tt.c_str()), 21 ) << endl;
+	cout << endl << "以下為 Python: " << endl;
+	system("python python_base64_sha1.py");
 
 	return 0;
 }
--- python_base64_sha1.py
+++ python_base64_sha1.py
@@ -6,14 +6,10 @@ sha1=hashlib.sha1()
 
 sha1.update("scott")
 
-for i in xrange(len(sha1.digest())):
-	print sha1.digest()[i].encode('hex'),
-print
-
-print sha1.digest()
+# print 'sha1:',
+# for i in xrange(len(sha1.digest())):print sha1.digest()[i].encode('hex'),
+# print ''
 
 SWSA=base64.b64encode(sha1.digest())
 
-print '單純看 base64(scott to): '+base64.b64encode('scott')
-
-print SWSA
+print '原先(\'scott\' sha1 then base64): '+SWSA
