0f6dcc19b93b72902db07343d7d0613e849725c1
scott1028 <mic1028002@gmail.com>
2013-11-12 13:19:42 +0800

    modified:   libwebsocket.hpp

--- libwebsocket.hpp
+++ libwebsocket.hpp
@@ -55,7 +55,7 @@ namespace network {
 					fd_and_mutex kk;
 					kk.newSocketFD=newSocketFD;
 
-					// 產生執行續, recv/send handle, // (void*)&newSocketFD
+					// 產生執行緒, recv/send handle, // (void*)&newSocketFD
 					if( 0 != pthread_create(&thread_recv, NULL, Websocket::recvThread, (void*)&kk ) ){	perror("Error creating thread"); };
 					if( 0 != pthread_create(&thread_send, NULL, Websocket::sendThread, (void*)&kk ) ){	perror("Error creating thread"); };
 				}
