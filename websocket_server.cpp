68cea4524a3eced90c4e9968236ef6ea94ecae78
scott1028 <mic1028002@gmail.com>
2013-11-13 14:55:29 +0800

    modified:   websocket_server.cpp

--- websocket_server.cpp
+++ websocket_server.cpp
@@ -1,8 +1,13 @@
 /*
 	base C++ WebSocket Server for GCC 3.3+ ( windows, centos )；GCC 3.4.5 ( Linux CCT-Linux 2.6.18v2 )
-	compiler: gcc websocket_server.cpp -lstdc++ -lpthread -Wall
+
+	compiler: 
+		gcc websocket_server.cpp -lstdc++ -lpthread -Wall -o ws && ./ws.exe
+		gcc websocket_server.cpp -lstdc++ -lpthread -Wall -o ws && ./ws
+
 	Author: Scott
-	Work for Cygwin、Centos 6.2
+	
+	note: Work for Cygwin、Centos 6.2
 */
 
 #include <iostream>
